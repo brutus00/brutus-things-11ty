32d4b983dc11a3361c79453886354af462bc6f32
Mitchell Christ <xzmaru@gmail.com>
2020-11-21 21:24:56 -0800

    update description

--- src/data/site.js
+++ src/data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: "Brutus' Pretty Good Handmade Things",
-  description: "Brutus' pens suck",
+  description: "Brutus' website of pretty good things that are handmade.",
   url: 'https://brutus-things-11ty.netlify.com'
 }
